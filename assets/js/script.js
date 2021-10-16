ffae9a933d175a72283db7373c09ec0446e0b1fd
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2021-10-15 23:45:33 -0700

    Completed the js file and made the planner ready for use

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,47 @@
 // Save the input of each timeblock in localstorage
 
 // Get the today's date
-var today = moment().format("dddd, MMMM Do YYYY")
-$("#currentDay").text(today);
 
+$(document).ready(function () {
+  var today = moment().format("dddd, MMMM Do YYYY");
+  $("#currentDay").text(today);
 
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
 
+    localStorage.setItem(time, text);
+  });
 
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 
+  function colorCode() {
+    var currentHour = moment().hour();
 
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id"));
 
+      if (currentHour > blockHour) {
+        $(this).children(".description").addClass("past");
+      } else if (currentHour === blockHour) {
+        $(this).children(".description").removeClass("past");
+        $(this).children(".description").addClass("present");
+      } else {
+        $(this).children(".description").removeClass("past");
+        $(this).children(".description").removeClass("present");
+        $(this).children(".description").addClass("future");
+      }
+    });
+  }
 
- var userInputEl = $("textarea")
+  colorCode();
+});
 
-function saveUserInput(){
-    localStorage.setItem("userEvent", userInputEl)
-}
-
-
-
-var saveButton = $(".saveBtn")
-saveButton.on("click", saveUserInput)
-  
